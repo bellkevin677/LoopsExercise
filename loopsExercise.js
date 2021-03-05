7d41818b4830c7f7b51895aa7c9607d4e9dc8d49
bellkevin677 <bellkevin677@gmail.com>
2021-03-05 12:48:54 -0600

    all except last bonus

---
+++ loopsExercise.js
@@ -0,0 +1,49 @@
+for (i = 1; i <= 7; i++){
+    console.log(i);
+}
+
+for (i = 5; i <= 25; i += 5){
+    console.log(i);
+}
+
+const wizards = [
+    "Harry Potter",
+    "Hermione Granger",
+    "Ron Weasley"
+]
+
+for (n of wizards){
+    console.log(n);
+}
+
+let harryPotterMovies = 0
+
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+}
+
+const hogwartHouses = [
+    "Gryffindor",
+    "Hufflepuff",
+    "Ravenclaw",
+    "Slytherin"
+]
+
+for (house of hogwartHouses){
+    for (char of house){
+        console.log(char);
+    }
+}
+
+const quote = [
+    "Yer",
+    "A",
+    "Wizard",
+    "Harry"
+];
+
+for (word of quote){
+    console.log(word);
+}
+
